de581fb351967725d33505d5e2fbe851bfb0cd08
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-05-27 11:20:21 +0300

    create componets Search,Messages,ContactList

---
+++ src/components/ContactList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Search from './Search';
+
+
+
+class ContactList extends React.Component {
+
+    render (){
+        return (
+            <div >
+                <Search/>
+            </div>
+    )
+
+
+    }
+
+
+
+    }
+
+    export default ContactList;
