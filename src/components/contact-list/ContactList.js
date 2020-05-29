eeba246c0a408ed6cdb6d835ad0c6c2826500a06
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-05-29 18:36:39 +0300

    add style effects& icons, add responsive styles

--- src/components/contact-list/ContactList.js
+++ src/components/contact-list/ContactList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './ContactList.css'
+import './ContactList.css';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 
 
@@ -20,33 +21,66 @@ class ContactList extends React.Component {
         return (
             <div  className="contact-block">
                 <div className="search-block">
+
                     <div className="logo">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSin-Oq8qcKN7sGLnEkgUpWQWUgCyQmK1Y7HHY5R6Lt-N-PQ-ZO&usqp=CAU" alt=""/>
+                            <div className="logo-img">
+                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSin-Oq8qcKN7sGLnEkgUpWQWUgCyQmK1Y7HHY5R6Lt-N-PQ-ZO&usqp=CAU" alt=""/>
+                            </div>
+                        <div className="checked-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512" width="20px">
+                                <g>
+                                    <path
+                                        d="m256 512c-141.164062 0-256-114.835938-256-256s114.835938-256 256-256 256 114.835938 256 256-114.835938 256-256 256zm0-480c-123.519531 0-224 100.480469-224 224s100.480469 224 224 224 224-100.480469 224-224-100.480469-224-224-224zm0 0"
+                                        data-original="#000000" className="active-path" data-old_color="#000000"
+                                        fill="#17A30C"/>
+                                    <path
+                                        d="m232 341.332031c-4.097656 0-8.191406-1.554687-11.308594-4.691406l-69.332031-69.332031c-6.25-6.253906-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l58.023437 58.027344 127.363281-127.359375c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.632812l-138.667969 138.667969c-3.15625 3.136719-7.25 4.691406-11.347656 4.691406zm0 0"
+                                        data-original="#000000" className="active-path" data-old_color="#000000"
+                                        fill="#17A30C"/>
+                                </g>
+                            </svg>
+                        </div>
 
                     </div>
-                    <label htmlFor="contact-search"> </label>
+
+                    <div className="search-wrapper">
+                    <i className="material-icons" id="search-icon">search</i>
 
                     <input type="text"
                        id="contact-search"
                        placeholder= "Search or start new chart"
                        onChange={this.searchData}
 
-
                     />
-
+                    </div>
                 </div>
 
                 <div className="contact-list">
                     <p id="chats">Chats</p>
+                    <Scrollbars  style={{ width: '100%', height: '75vh'}}>
 
-                    <div className="list">
 
 
                         {MessageFolder.map((item)=>{
                             return (<div className="contact-profile" key={item.id} onClick={()=>{currentContactData(item)}}>
                                 <div className="logo">
+                                    <div className="logo-img">
                                     <img src={item.img} alt={item.name}/>
-                                    <span className="checked-icon" > </span>
+                                    </div>
+                                    <div className="checked-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512" width="20px">
+                                            <g>
+                                                <path
+                                                    d="m256 512c-141.164062 0-256-114.835938-256-256s114.835938-256 256-256 256 114.835938 256 256-114.835938 256-256 256zm0-480c-123.519531 0-224 100.480469-224 224s100.480469 224 224 224 224-100.480469 224-224-100.480469-224-224-224zm0 0"
+                                                    data-original="#000000" className="active-path" data-old_color="#000000"
+                                                    fill="#17A30C"/>
+                                                <path
+                                                    d="m232 341.332031c-4.097656 0-8.191406-1.554687-11.308594-4.691406l-69.332031-69.332031c-6.25-6.253906-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l58.023437 58.027344 127.363281-127.359375c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.632812l-138.667969 138.667969c-3.15625 3.136719-7.25 4.691406-11.347656 4.691406zm0 0"
+                                                    data-original="#000000" className="active-path" data-old_color="#000000"
+                                                    fill="#17A30C"/>
+                                            </g>
+                                        </svg>
+                                    </div>
                                 </div>
                                 <div  className= 'profile-text-block'  >
                                     <p className='contact-name'>{item.name} </p>
@@ -68,9 +102,9 @@ class ContactList extends React.Component {
 
 
 
-                    </div>
 
 
+                        </Scrollbars >
 
 
 
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import  './Messages.css';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 
 
@@ -70,13 +71,30 @@ class Messages extends React.Component {
                 { activeContact.message ?  (<div>
                     <div className="current-contact">
                         <div className="logo">
-                            <img src={activeContact.img} alt={activeContact.name}/>
+                            <div className="logo-img">
+                                <img src={activeContact.img} alt={activeContact.name}/>
+                            </div>
+                            <div className="checked-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512" width="20px">
+                                    <g>
+                                        <path
+                                            d="m256 512c-141.164062 0-256-114.835938-256-256s114.835938-256 256-256 256 114.835938 256 256-114.835938 256-256 256zm0-480c-123.519531 0-224 100.480469-224 224s100.480469 224 224 224 224-100.480469 224-224-100.480469-224-224-224zm0 0"
+                                            data-original="#000000" className="active-path" data-old_color="#000000"
+                                            fill="#17A30C"/>
+                                        <path
+                                            d="m232 341.332031c-4.097656 0-8.191406-1.554687-11.308594-4.691406l-69.332031-69.332031c-6.25-6.253906-6.25-16.386719 0-22.636719s16.382813-6.25 22.636719 0l58.023437 58.027344 127.363281-127.359375c6.25-6.25 16.382813-6.25 22.632813 0s6.25 16.382812 0 22.632812l-138.667969 138.667969c-3.15625 3.136719-7.25 4.691406-11.347656 4.691406zm0 0"
+                                            data-original="#000000" className="active-path" data-old_color="#000000"
+                                            fill="#17A30C"/>
+                                    </g>
+                                </svg>
+                            </div>
+
 
                         </div>
                         <div className="name">  {activeContact.name}  </div>
                     </div>
 
-
+                    <Scrollbars style={{ width: '100%', height: '76vh' }}>
                     <div className="chat">
                         {  activeContact.message.map(item=>{
                             return(<div>
@@ -88,7 +106,9 @@ class Messages extends React.Component {
 
                                     (<div className="contact-message-wrapper">
                                         <div className="logo">
+                                            <div className="logo-img">
                                             <img src={activeContact.img} alt={activeContact.name}/>
+                                            </div>
                                         </div>
 
                                         <p className="contact-message"> {item.text} </p>
@@ -98,11 +118,12 @@ class Messages extends React.Component {
                             </div>)
                         })
 
-                        }</div>
+                        } </div>
 
+                        </Scrollbars>
 
                 <div className="new-message-block">
-
+                <div id="new-message-wrapper">
                 <input
                 type="text"
                 id="new-message"
@@ -110,12 +131,13 @@ class Messages extends React.Component {
                 onChange={this.newMessageToState}
                 placeholder="Type your message"
                 />
+
                 <button
                 id="send-message"
                 onClick={this.sendNewMessage}>
                 <i className="material-icons" id="send-button">send</i>
                 </button>
-
+                </div>
 
                 </div>
 
