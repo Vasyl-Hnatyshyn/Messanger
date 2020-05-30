b193a8c6f289c654912b35dcc3ef2a9edbc573f1
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-05-30 16:40:10 +0300

    Updates

---
+++ precache-manifest.5ea4ba125258d7067dfc8e91c5d02abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dc2458ddd85fa7e31490f99b896ada",
+    "url": "/Messanger/index.html"
+  },
+  {
+    "revision": "0c78d3ec710df3f263fc",
+    "url": "/Messanger/static/css/main.d9656c53.chunk.css"
+  },
+  {
+    "revision": "dc9714573a58a1b1367b",
+    "url": "/Messanger/static/js/2.46d32491.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Messanger/static/js/2.46d32491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c78d3ec710df3f263fc",
+    "url": "/Messanger/static/js/main.f16c868d.chunk.js"
+  },
+  {
+    "revision": "a4cad3a7f04a167c8ac3",
+    "url": "/Messanger/static/js/runtime-main.cb9c715b.js"
+  }
+]);
